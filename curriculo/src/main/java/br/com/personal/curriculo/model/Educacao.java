532a841fe5b5428a7015d4c8a555cf90ae274d23
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-05 21:04:02 -0300

    adicionado: classe Pessoa

--- curriculo/src/main/java/br/com/personal/curriculo/model/Educacao.java
+++ curriculo/src/main/java/br/com/personal/curriculo/model/Educacao.java
@@ -1,5 +1,24 @@
 package br.com.personal.curriculo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Educacao {
     
+    private String nomeCurso;
+    private String tipoEducacao;
+    private String instituicao;
+    private int anoConclusao;
+
+    @Override
+    public String toString() {
+        return String.format("""
+                %s - %s
+                %d - %s
+        """, tipoEducacao, nomeCurso, anoConclusao, instituicao);
+    }
 }
