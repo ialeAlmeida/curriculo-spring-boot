1121e052f3645e02c3aaf601bc8b25d3e4a47e18
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-05 21:04:36 -0300

    adicionado: classe Exeperiencia

--- curriculo/src/main/java/br/com/personal/curriculo/model/Experiencia.java
+++ curriculo/src/main/java/br/com/personal/curriculo/model/Experiencia.java
@@ -1,5 +1,24 @@
 package br.com.personal.curriculo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Experiencia {
     
+    private String nomeExperiencia;
+    private String instituicao;
+    private String periodoConclusao;
+    private String descricao;
+    
+    public String toString() {
+        return String.format("""
+                %s - %s
+                %s
+                %s
+                """, instituicao, nomeExperiencia, periodoConclusao, descricao);
+    }
 }
