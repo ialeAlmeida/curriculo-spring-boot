2f0c9c2bf33b64e147ce910a3f30a375d1da42d2
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-05 21:07:24 -0300

    adicionado: classe Habilidade

--- curriculo/src/main/java/br/com/personal/curriculo/model/Habilidade.java
+++ curriculo/src/main/java/br/com/personal/curriculo/model/Habilidade.java
@@ -1,5 +1,14 @@
 package br.com.personal.curriculo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Habilidade {
     
+    private String nome;
+
 }
